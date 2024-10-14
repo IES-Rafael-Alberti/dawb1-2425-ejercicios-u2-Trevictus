026b3c1919a22e4babb8a65fbb4fc8345be0242a
vgomtej353 <gtvictor@iesrafaelalberti.es>
2024-10-14 21:24:44 +0200

    Planteamiento U2

---
+++ src/actividad1.py
@@ -0,0 +1,2 @@
+"""Actividad 1: Escribe un programa que lea repetidamente números hasta que el usuario introduzca “fin”. Una vez se haya introducido “fin”, muestra por pantalla el total, la cantidad de números y la media de esos números. """
+
