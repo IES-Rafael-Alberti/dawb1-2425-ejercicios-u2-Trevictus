2b0abe4e33851933f858da5b4749aca86211ab3c
vgomtej353 <gtvictor@iesrafaelalberti.es>
2024-10-17 12:12:21 +0200

    Primer try/except que hago con éxito

--- src/actividad1.py
+++ src/actividad1.py
@@ -1,2 +1,24 @@
 """Actividad 1: Escribe un programa que lea repetidamente números hasta que el usuario introduzca “fin”. Una vez se haya introducido “fin”, muestra por pantalla el total, la cantidad de números y la media de esos números. """
 
+def introducir_numeros():
+    encontrar_fin = False
+    lista_numeros = []
+
+    while not encontrar_fin:
+        numeros = input("Introduce nºs y escribe fin para finalizar: ")
+        
+        if numeros == "fin":
+            encontrar_fin = True
+        else:
+            try:
+                lista_numeros.append(int(numeros))
+            except ValueError:
+                print("Por favor introduce nºs válidos.")
+            
+    print(f"Suma del total: {sum(lista_numeros)}\nTotal de nºs: {len(lista_numeros)}\nMedia total: {sum(lista_numeros)/len(lista_numeros)}")
+
+def main():
+    introducir_numeros()
+
+if __name__ == "__main__":
+    main()
--- src/p2.1.1.py
+++ src/p2.1.1.py
@@ -3,3 +3,17 @@
 Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
 
 """
+
+def comprobar_edad(edad: int)-> str:
+    if edad >= 18:
+        return f"Con la edad de {edad} eres mayor."
+    else:
+        return f"Con la edad de {edad} eres menor."
+    
+def main():
+    edad = int(input("Ingresa tu edad: "))
+    alerta = comprobar_edad(edad)
+    print(alerta)
+
+if __name__ == "__main__":
+    main()
