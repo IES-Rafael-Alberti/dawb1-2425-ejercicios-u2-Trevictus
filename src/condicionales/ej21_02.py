de1e394c66b810538111ac9ca0d0b908f7a0af4c
vgomtej353 <gtvictor@iesrafaelalberti.es>
2024-10-17 21:38:46 +0200

    Oleeee

---
+++ src/condicionales/ej21_02.py
@@ -0,0 +1,24 @@
+# Ejercicio 2.1.2
+"""Escribir un programa que almacene la cadena de caracteres contraseña en una variable, pregunte al usuario por la contraseña e imprima por pantalla si la contraseña introducida por el usuario coincide con la guardada en la variable sin tener en cuenta mayúsculas y minúsculas."""
+
+CONTRASENIA = "contraseña"
+
+def pedir_contrasenia():
+    return input("¿Cuál es la contraseña?\n").lower()
+
+
+def comprobar_pass(password: str) -> bool:
+    return password == CONTRASENIA
+
+
+def main():
+    cont_introducida = pedir_contrasenia()
+
+    if comprobar_pass(cont_introducida):
+        print(f"La contraseña {cont_introducida} coincide.")
+    else:
+        print(f"La contraseña { cont_introducida} no coincide con la contraseña.")
+
+
+if __name__ == "__main__":
+    main()
