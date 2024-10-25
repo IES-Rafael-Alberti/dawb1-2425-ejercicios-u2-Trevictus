a30b4c6180ea1e0270c8bec9b6aeb0f3735cd648
vgomtej353 <gtvictor@iesrafaelalberti.es>
2024-10-25 17:54:51 +0200

    Ejercicio 22_13

--- src/iterativas_saltos/ej22_13.py
+++ src/iterativas_saltos/ej22_13.py
@@ -1,8 +1,27 @@
 # Ejercicio 2.2.13
 # Escribir un programa que muestre el eco de todo lo que el usuario introduzca hasta que el usuario escriba “salir” que terminará.
 
+def retornar_eco():
+    salir = False
+    eco = input("Escribe algo o 'salir' para finalizar el programa: ")
+    if eco == "salir":
+            print("Eco finalizado.")
+            salir = True
+    else:
+            print(eco)
+    while not salir:
+        eco = input()
+        if eco == "salir":
+            print("Eco finalizado.")
+            salir = True
+        else:
+            print(eco)
+            
+
+
+
 def main():
-    return
+    retornar_eco()
 
 if __name__ == "__main__":
     main()
