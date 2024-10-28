fc54753ebd605797e7bc4c148c45b99971aef589
vgomtej353 <gtvictor@iesrafaelalberti.es>
2024-10-28 18:35:23 +0100

    Algoritmo burbuja comprendido

--- src/depuracion/ej24_01.py
+++ src/depuracion/ej24_01.py
@@ -1,8 +1,17 @@
 # Ejercicio 2.4.1
 # Algoritmo burbuja
 
+def burbuja(lista): 
+    n = len(lista) #Longitud de la lista
+    for i in range(n): #Se recorre cada elemento de la lista n
+        for j in range(0, n-i-1): #A cada elemento se le asigna una posicion en la variable j
+            if lista[j] > lista[j+1]: #Si el valor de la posicion es mas alto que su siguiente
+                lista[j], lista[j+1] = lista[j+1], lista[j]#Se invierte el orden de los dos elementos comparados
+    return lista                                           #Se vuelve a comenzar el recorrido de los elementos por por la lista n
+
 def main():
-    return
+    n = [45, 12, 76, 11, 1, 234, 32, 33, 34, 6]
+    print(burbuja(n))
 
 if __name__ == "__main__":
     main()
