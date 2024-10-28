738d806a0604dde13c09e1b96c42593b5787d1de
vgomtej353 <gtvictor@iesrafaelalberti.es>
2024-10-28 21:21:40 +0100

    Indicios de prueba

--- src/iterativas_saltos/ej22_19.py
+++ src/iterativas_saltos/ej22_19.py
@@ -3,22 +3,34 @@
 # Si elige una opción incorrecta, informarle del error. El menú se debe volver a mostrar luego de ejecutada cada opción, permitiendo volver a elegir. 
 # Si elige las opciones 1 ó 2 se imprimirá un texto. Si elige la opción 3, se interrumpirá la impresión del menú y el programa finalizará.
 
+from os import system, name
+
 def menu():
     listado = ("Goma", "Lápiz", "Papel")
     finalizar = False
+    clear()
     while not finalizar:
         try:   
             opcion = int(input("Ingresa opción:\n1-> Comenzar programa.\n2-> Imprimir listado.\n3-> Finalizar programa.\n=>"))
             if opcion == 1:
-                return "Programa comenzado."
+                clear()
+                print( "Programa comenzado.")
             elif opcion == 2:
-                return f"El listado es el siguiente: {", ".join(listado)}"
+                clear()
+                print( f"El listado es el siguiente: {", ".join(listado)}")
             elif opcion == 3:
+                clear()
+                print("Aaaadios!")
                 finalizar = True
             else:
                 raise ValueError("ERROR. Ingresa una opción")
         except ValueError as _:
+            clear()
             print(f"ERROR. Ingresa una opción válida")
+
+def clear():
+    if name == 'nt':
+        _ = system('cls')
 
 def main():
     print(menu())
---
+++ src/prueba29_10_24.py
@@ -0,0 +1,36 @@
+"""adivina numero
+entre 0 y 100
+numero intento 5
+frio frio el numero es mayor-menor
+caliente caliente mayor-menor
+
+
+
+
+menu cinco opciones
+configurar
+jugar
+mostrar configuracion
+salir
+
+#elige
+
+tupla con secciones de titulo"""
+
+def presentacion():
+    return "Bienvenido a Adivinar Nºs!!!"
+
+def validar_numeros():
+
+def generar_numero_oculto():
+
+def configurar_rango():
+
+def menu():
+
+def mostrar_configuracion():
+
+
+
+def main():
+    presentacion()
